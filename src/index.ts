7a914f88b3ab652ab6c63ff22708966798c3542f
Andrew Cordiner <andrewcordiner92@gmail.com>
2021-08-19 23:14:20 +1000

    Wired up the dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import { runAudit } from './auditPipeline';
+import { exec } from 'child_process';
+import { AuditPipelineEnv, runAudit } from './auditPipeline';
+import { ExecutorEnv, runNpmAuditCommand } from './executor';
+
+const executorEnv: ExecutorEnv = {
+  exec,
+};
+
+const auditPipelineEnv: AuditPipelineEnv = {
+  getCommandLineArgs: () => process.argv.slice(2),
+  runNpmAuditCommand: config => runNpmAuditCommand(config)(executorEnv),
+  exitProcess: (status: number) => process.exit(status),
+};
 
 // eslint-disable-next-line fp/no-unused-expression
-void runAudit()();
+void runAudit()(auditPipelineEnv)();
